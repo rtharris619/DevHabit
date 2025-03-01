4dcd15ed1db8278e853955518826ca278c4dd7cd
Ryan Harris <ryan@synthesis.co.za>
2025-03-01 16:01:38 +0200

    habit db and controller hookup

---
+++ DevHabit/DevHabit.Api/Controllers/HabitsController.cs
@@ -0,0 +1,101 @@
+﻿using DevHabit.Api.Database;
+using DevHabit.Api.DTOs.Habits;
+using DevHabit.Api.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevHabit.Api.Controllers;
+
+[ApiController]
+[Route("habits")]
+public sealed class HabitsController(ApplicationDbContext dbContext) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<HabitsCollectionDto>> GetAsync()
+    {
+        List<HabitDto> habits = await dbContext
+            .Habits
+            .Select(h => new HabitDto
+            {
+                Id = h.Id,
+                Name = h.Name,
+                Description = h.Description,
+                Type = h.Type,
+                Frequency = new FrequencyDto
+                {
+                    Type = h.Frequency.Type,
+                    TimesPerPeriod = h.Frequency.TimesPerPeriod
+                },
+                Target = new TargetDto
+                {
+                    Value = h.Target.Value,
+                    Unit = h.Target.Unit
+                },
+                Status = h.Status,
+                IsArchived = h.IsArchived,
+                EndDate = h.EndDate,
+                Milestone = h.Milestone == null ? null : new MilestoneDto
+                {
+                    Target = h.Milestone.Target,
+                    Current = h.Milestone.Current
+                },
+                CreatedAtUtc = h.CreatedAtUtc,
+                UpdatedAtUtc = h.UpdatedAtUtc,
+                LastCompletedAtUtc = h.LastCompletedAtUtc
+            })
+            .ToListAsync();
+
+        var habitsCollectionDto = new HabitsCollectionDto
+        {
+            Data = habits
+        };
+
+        return Ok(habitsCollectionDto);
+    }
+
+    [HttpGet]
+    [Route("{id}")]
+    public async Task<ActionResult<HabitDto>> GetByIdAsync(string id)
+    {
+        HabitDto habit = await dbContext
+            .Habits
+            .Where(h => h.Id == id)
+            .Select(h => new HabitDto
+            {
+                Id = h.Id,
+                Name = h.Name,
+                Description = h.Description,
+                Type = h.Type,
+                Frequency = new FrequencyDto
+                {
+                    Type = h.Frequency.Type,
+                    TimesPerPeriod = h.Frequency.TimesPerPeriod
+                },
+                Target = new TargetDto
+                {
+                    Value = h.Target.Value,
+                    Unit = h.Target.Unit
+                },
+                Status = h.Status,
+                IsArchived = h.IsArchived,
+                EndDate = h.EndDate,
+                Milestone = h.Milestone == null ? null : new MilestoneDto
+                {
+                    Target = h.Milestone.Target,
+                    Current = h.Milestone.Current
+                },
+                CreatedAtUtc = h.CreatedAtUtc,
+                UpdatedAtUtc = h.UpdatedAtUtc,
+                LastCompletedAtUtc = h.LastCompletedAtUtc
+
+            })
+            .FirstOrDefaultAsync();
+
+        if (habit is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(habit);
+    }
+}
---
+++ DevHabit/DevHabit.Api/Extensions/DatabaseExtensions.cs
@@ -0,0 +1,25 @@
+﻿using DevHabit.Api.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevHabit.Api.Extensions;
+
+public static class DatabaseExtensions
+{
+    public static async Task ApplyMigrationsAsync(this WebApplication app)
+    {
+        using IServiceScope scope = app.Services.CreateScope();
+        await using ApplicationDbContext context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+        try
+        {
+            await context.Database.MigrateAsync();
+
+            app.Logger.LogInformation("Database migrations applied successfully.");
+        }
+        catch (Exception ex)
+        {
+            app.Logger.LogError(ex, "An error occurred while applying database migrations.");
+            throw;
+        }
+    }
+}
