7544aadce787cfa712f8b572a4a7b471a9a6a835
Ryan Harris <ryan@synthesis.co.za>
2025-03-05 17:58:22 +0200

    Added tags

---
+++ DevHabit/DevHabit.Api/Controllers/TagsController.cs
@@ -0,0 +1,96 @@
+﻿using DevHabit.Api.Database;
+using DevHabit.Api.DTOs.Habits;
+using DevHabit.Api.DTOs.Tags;
+using DevHabit.Api.Entities;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevHabit.Api.Controllers;
+
+[ApiController]
+[Route("tags")]
+public sealed class TagsController(ApplicationDbContext dbContext) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<TagsCollectionDto>> GetTags()
+    {
+        List<TagDto> tags = await dbContext
+            .Tags
+            .Select(TagQueries.ProjectToDto())
+            .ToListAsync();
+
+        var tagsCollectionDto = new TagsCollectionDto
+        {
+            Data = tags
+        };
+
+        return Ok(tagsCollectionDto);
+    }
+
+    [HttpGet]
+    [Route("{id}")]
+    public async Task<ActionResult<TagDto>> GetTag(string id)
+    {
+        TagDto tag = await dbContext
+            .Tags
+            .Where(h => h.Id == id)
+            .Select(TagQueries.ProjectToDto())
+            .FirstOrDefaultAsync();
+
+        if (tag is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(tag);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<HabitDto>> CreateTag(CreateTagDto dto)
+    {
+        Tag tag = dto.ToEntity();
+
+        dbContext.Tags.Add(tag);
+
+        await dbContext.SaveChangesAsync();
+
+        TagDto tagDto = tag.ToDto();
+
+        return CreatedAtAction(nameof(GetTag), new { id = tagDto.Id }, tagDto);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult> UpdateTag(string id, UpdateTagDto dto)
+    {
+        Tag? tag = await dbContext.Tags.FirstOrDefaultAsync(t => t.Id == id);
+
+        if (tag is null)
+        {
+            return NotFound();
+        }
+
+        tag.UpdateFromDto(dto);
+
+        await dbContext.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteTag(string id)
+    {
+        Tag? tag = await dbContext.Tags.FirstOrDefaultAsync(t => t.Id == id);
+
+        if (tag is null)
+        {
+            return NotFound();
+        }
+
+        dbContext.Tags.Remove(tag);
+
+        await dbContext.SaveChangesAsync();
+
+        return NoContent();
+    }
+}
